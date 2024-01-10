2e800f8263c5167e1109f4a181bf9fc651e2367d
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2024-01-10 23:52:51 +0545

    The initial splash screen implemented

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <raymath.h>
 
 #include <stdint.h>
-
+#include "splashScreen.cpp"
 struct Window{
     int w,h;
 };
@@ -19,6 +19,13 @@ int main(void){
     };
     InitWindow(width, height, "The Lost Spectrum");
 
+
+    splashScreen ss("TEAM DOTS PRESENTS", width, height);
+    ss.displaySplashScreen(BLACK, RAYWHITE);
+    
+    splashScreen ss2("THE LOST SPECTRUM", width, height);
+    ss2.displaySplashScreen(WHITE, BLACK);
+
     SetTargetFPS(60);
 
     float accumulator = 0;
@@ -29,6 +36,7 @@ int main(void){
         
         // fixed dt for fixed time updates
         const float FIXED_DT = 0.002f;
+
         while(accumulator >= FIXED_DT){
             // update physics and stuff
 
@@ -37,16 +45,14 @@ int main(void){
 
         BeginDrawing();
         Color background = {
-            255,255,0,255
+            0,0,0,255
         };
 
         ClearBackground(background);
-
+        DrawText("INITIAL CONVERSATION STARTS", static_cast<int>((width/2)-250), static_cast<int>(height/2), 50, WHITE);
+        
         
         EndDrawing();
-
-
-
 
     }
     return 0;
