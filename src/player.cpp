0c28c4b8012685a5c8db6347cfe42216898f6403
Ishwor Basyal <ishwor8basyal@gmail.com>
2024-01-11 18:26:40 +0545

    player moves

---
+++ src/player.cpp
@@ -0,0 +1,21 @@
+#include <raylib.h>
+class player
+{
+	Vector2 position = { 120,120 };
+	float speed=0;
+	Vector2 size = { 16,16 };
+
+public:
+	void movementCheck()
+	{
+		if (IsKeyDown(KEY_RIGHT)) position.x += 5;
+		if (IsKeyDown(KEY_LEFT)) position.x -= 5;
+		if (IsKeyDown(KEY_DOWN)) position.y += 5;
+		if (IsKeyDown(KEY_UP)) position.y -= 5;
+	}
+	void draw()
+	{
+		DrawRectangleV(position, size, RED);
+	}
+	
+};
