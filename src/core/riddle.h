307733d2e472bda8c753d2fa77543f7f84afb085
Ishwor Basyal <ishwor8basyal@gmail.com>
2024-01-13 13:06:34 +0545

    Minor changes in riddle

--- src/core/riddle.h
+++ src/core/riddle.h
@@ -1,19 +1,23 @@
 #pragma once
 #include<raylib.h>
 #include "dialogue.h"
+#include <iostream>
 
 class riddle
 {
 	const float PADDING = 10;
+	const int riddleDialogueSize = 3;
+	const int riddleTextSize = 4;
 	Rectangle characterRect;
-	bool empty = false;
+	bool winDisplay,riddleDisplay,empty = false;	
 
 	Rectangle dialogueBoxRect;
 	Rectangle dialogueRect;
-	char riddleDialogue[4][100] =
+	char riddleDialogue[3][100] =
 	{
-		"Hey there, fearless adventurer! So, you've bested the boss and stumbled upon",
-		"the Chromalock, the bouncer of the rainbow realm. To crack its colorful code, think in rhymes"
+		"Hey there, fearless adventurer!",
+		"So, you've bested the boss and stumbled uponthe Chromalock, the bouncer of the rainbow realm.",
+		"To crack its colorful code, think in rhymes"
 	};
 	char riddleText[4][100]=
 	{
@@ -21,7 +25,6 @@ class riddle
 		"A device conceals us in a play",
 		"Search everywhere- a key it holds",
 		"Retrieve it to see the colors unfold"
-
 	};
 
 public:
@@ -30,18 +33,31 @@ public:
 	{
 		dialogueBoxRect = { PADDING, windowH * 0.7f - PADDING, windowW - 2 * PADDING, windowH * 0.3f };
 		DrawRectangleRec(dialogueBoxRect, Color{ 64,64,64,128 });
-		for (int i = 0; i < 2; i++)
+		//if (winDisplay==false)
+		//{
+			std::cout << "Inside" << std::endl;
+			for (int i = 0; i < 2; i++)			
+				DrawText(TextFormat(riddleDialogue[i]), dialogueBoxRect.x + PADDING, dialogueBoxRect.y + i * 20 + PADDING, 20, WHITE);
+		//}
+		//winDisplay = true;		
+		/*if (!riddleDisplay)
 		{
-			DrawText(TextFormat(riddleDialogue[i]), dialogueBoxRect.x + PADDING, dialogueBoxRect.y + PADDING, 20, WHITE);
+			for (int i = 0; i < riddleTextSize; i++)
+				DrawText(TextFormat(riddleText[i]), dialogueBoxRect.x + PADDING, dialogueBoxRect.y + i * 20 + PADDING, 20, WHITE);
+			
+			
+		}*/
+		riddleDisplay = true;
+		winDisplay = true;
 		
-		}
-		empty = true;
+		
 			//DrawText(TextFormat(riddleDialogue[i]),dialogueBoxRect.x+PADDING,dialogueBoxRect.y+PADDING,20,WHITE);
 			//DrawText(riddleDialogue[0], dialogueBoxRect.x + PADDING, dialogueBoxRect.y + PADDING, 20, WHITE);
 	}
 	bool isEmpty()
 	{
-		return empty;
+		if(riddleDisplay && winDisplay)
+			return !empty;
 	}
 
 
