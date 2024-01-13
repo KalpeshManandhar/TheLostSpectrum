d7ca12ee28077c066a6c28644cf5d8801dced2fc
Ishwor Basyal <ishwor8basyal@gmail.com>
2024-01-13 19:15:36 +0545

    Added the file handling for riddle

--- src/core/riddle.h
+++ src/core/riddle.h
@@ -2,15 +2,16 @@
 #include<raylib.h>
 #include "dialogue.h"
 #include <iostream>
+#include <fstream>
 
 
 
 class inputBox
 {
 	int screenWidth = GetScreenWidth();
-	int screenHeight = GetScreenHeight();
+	int screenHeight = GetScreenHeight();	
 
-	char name[10 + 1] = "\0";
+	char answer[10 + 1] = "\0";
 	int letterCount = 0;
 
 	Rectangle textBox = { screenWidth / 2.0f - 100, 180, 225, 50 };
@@ -32,8 +33,8 @@ public:
 			// NOTE: Only allow keys in range [32..125]
 			if ((key >= 32) && (key <= 125) && (letterCount < 10))
 			{
-				name[letterCount] = (char)key;
-				name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
+				answer[letterCount] = (char)key;
+				answer[letterCount + 1] = '\0'; // Adding null terminator at the end of the string.
 				letterCount++;
 			}
 
@@ -44,7 +45,7 @@ public:
 		{
 			letterCount--;
 			if (letterCount < 0) letterCount = 0;
-			name[letterCount] = '\0';
+			answer[letterCount] = '\0';
 		}
 
 		else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
@@ -59,18 +60,24 @@ public:
 		if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
 		else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
 
-		DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
+		DrawText(answer, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
 
-		DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, 10), 315, 250, 20, DARKGRAY);
+		DrawText(TextFormat("INPUT: %i/%i", letterCount, 10), 315, 250, 20, DARKGRAY);
 
 		if (mouseOnText)
 		{
 			if (letterCount < 10)
 			{
 				// Draw blinking underscore char
-				if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, MAROON);
+				if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(answer, 40), (int)textBox.y + 12, 40, MAROON);
 			}
-			else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
+			else DrawText("Press BACKSPACE to delete", 230, 300, 20, GRAY);
+		}
+		if (IsKeyPressed(KEY_ENTER))
+		{
+			std::cout << answer << '\n';
+			return;
+			
 		}
 	}
 
@@ -82,9 +89,12 @@ class riddle
 	const int riddleDialogueSize = 3;
 	const int riddleTextSize = 4;
 	Rectangle characterRect;
+
+	std::fstream FILE;
+	std::string filename = "test.riddle";
 	
 	inputBox box;
-	
+	bool file=true;
 	int state=0;
 	Rectangle dialogueBoxRect;
 	Rectangle dialogueRect;
@@ -103,26 +113,55 @@ class riddle
 	};
 
 public:
+	riddle()
+	{
+		if (file)
+		{
+			FILE.open(filename, std::ios::out);
+			if (!FILE)
+				std::cout << "ERROR";
+			else
+			{
+				std::cout << "YAY";
+				FILE << "THIS is the answer to the riddle";
+
+			}
+			FILE.close();
+			file = false;
+
+		}
+	}
+	~riddle()
+	{
+		int res=remove(filename.c_str());
+	}
 
 	void riddleBox(int windowW = 1280, int windowH = 720)
 	{
-		dialogueBoxRect = { PADDING, windowH * 0.7f - PADDING, windowW - 2 * PADDING, windowH * 0.3f };
-		DrawRectangleRec(dialogueBoxRect, Color{ 64,64,64,128 });
+		
 		std::cout << state << "\n";
+		
 		switch (state)
 		{
 		case 0:
+			dialogueBoxRect = { PADDING, windowH * 0.7f - PADDING, windowW - 2 * PADDING, windowH * 0.3f };
+			DrawRectangleRec(dialogueBoxRect, Color{ 64,64,64,128 });
 			//std::cout << "Inside" << std::endl;
 			for (int i = 0; i < riddleDialogueSize; i++)
 				DrawText(TextFormat(riddleDialogue[i]), dialogueBoxRect.x + PADDING, dialogueBoxRect.y + i * 20 + PADDING, 20, WHITE);
 			break;
 		case 1:
+			dialogueBoxRect = { PADDING, windowH * 0.7f - PADDING, windowW - 2 * PADDING, windowH * 0.3f };
+			DrawRectangleRec(dialogueBoxRect, Color{ 64,64,64,128 });
 			//std::cout << "Inside" << std::endl;
 			for (int i = 0; i < riddleTextSize; i++)
 				DrawText(TextFormat(riddleText[i]), dialogueBoxRect.x + PADDING, dialogueBoxRect.y + i * 20 + PADDING, 20, WHITE);
 			break;
 		case 2:
+			dialogueBoxRect = { PADDING, windowH * 0.7f - PADDING, windowW - 2 * PADDING, windowH * 0.3f };
+			DrawRectangleRec(dialogueBoxRect, Color{ 64,64,64,128 });
 			box.drawInputBox();
+
 			break;
 
 		default:
@@ -133,10 +172,12 @@ public:
 
 	bool choose()
 	{
+		if (state == 10)
+			return false;
 		state=(state+1)%4;
 		if (state == 3)
 		{
-			state = 0;
+			state = 10;
 			return false;
 		}
 		return true;
